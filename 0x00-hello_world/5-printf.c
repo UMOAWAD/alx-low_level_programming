ddbbc8e0177674fd3a40571d593b32c345c63411
Usama Enany <osama.enany@yahoo.com>
2023-08-10 10:33:38 -0700

    First C Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
- * returns 0
+/**
+ * main - prints "with proper grammar, 
+ * but the outcome is a piece of art," followed by a new line
+ * 
+ * Return: 0
  */
 
 int main(void)
