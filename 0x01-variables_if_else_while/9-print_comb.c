e14ced0fc3c325bfdb9ef5558d45ebd04a751c04
Usama Enany <osama.enany@yahoo.com>
2023-08-11 09:46:26 -0700

    Print Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int num1;
-	int num2;
 
 	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
+		if (num1 == '9')
 		{
 			putchar(num1);
-			putchar(num2);
+		} else
+		{
+			putchar(num1);
 			putchar(',');
 			putchar(' ');
 		}
