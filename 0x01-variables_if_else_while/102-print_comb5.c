8194edd2002b426f10cc114021d3d268c84f2dc4
Usama Enany <osama.enany@yahoo.com>
2023-08-11 12:19:13 -0700

    Two Two-digit Numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (num3 = '0'; num3 <= '9'; num3++)
+			{
+				for (num4 = '0'; num4 <= '9'; num4++)
+				{
+					if (num1 <= num3 && num2 < num4)
+					{
+					} else if (num2 <= num3)
+					{
+					putchar(num1);
+					putchar(num2);
+					putchar(' ');
+					putchar(num3);
+					putchar(num4);
+
+					if (num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9')
+					{
+						putchar('\n');
+					} else
+					{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return (0);
+}
