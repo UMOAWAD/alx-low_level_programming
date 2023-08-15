d1ab59a37a7731740068c4b8c46a0edabc6add26
Usama Enany <osama.enany@yahoo.com>
2023-08-15 12:21:38 -0700

    Alphabet Function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	
+	return (0);
+}
+
+int print_alphabet()
+{
+	char c;
+
+	for  (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+}
