1fc1e2aecd37ed8465b11740144a2c0cf250fc93
Usama Enany <osama.enany@yahoo.com>
2023-08-15 12:03:02 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,18 +8,25 @@
 
 int main(void)
 {
-	char put[] = "_putchar";
-	int i = 0;
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 'r';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+	char i = '\n';
 
-	for (i = 0; i < 9; i++)
-	{
-		putchar(put[i]);
-
-		if (i == 8)
-		{
-			putchar('\n');
-		}
-	}
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
 
 	return (0);
 }
