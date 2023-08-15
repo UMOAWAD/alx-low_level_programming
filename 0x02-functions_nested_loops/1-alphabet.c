a78199ebb58d824abba91ed400964d4fee8b980e
Usama Enany <osama.enany@yahoo.com>
2023-08-15 12:27:09 -0700

    Alphabet Function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - prints alphabet
  *
  * Return: 0
  */
-
-int main(void)
-{
-	print_alphabet();
-	
-	return (0);
-}
 
 void print_alphabet(void)
 {
