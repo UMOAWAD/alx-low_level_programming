2918f45a8e0595d692d3a844b70115118b6ed2d7
Usama Enany <osama.enany@yahoo.com>
2023-08-17 10:35:08 -0700

    IsLower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,16 @@
 /**
  * islower - prints 1 if c is lowercase
  *
- * Return: 0
+ * Return: 0 or 1
  */
 
 int _islower(int c)
 {
 	if (islower(c))
 	{
-		_putchar('1');
+		return(1);
 	} else
 	{
-		_putchar('0');
-	}
-
-	return (0);
+		return(0);
+	{
 }
