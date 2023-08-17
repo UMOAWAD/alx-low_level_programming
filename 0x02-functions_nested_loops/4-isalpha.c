64f39f97db61cd6e87bc3a95f5da80c06482b730
Usama Enany <osama.enany@yahoo.com>
2023-08-17 10:55:35 -0700

    IsAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - Returns 1 if the parameter is a letter.
+ *
+ * @c: The parameter to check.
+ *
+ * Return: 0 or 1
+ */
+
+int isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}
