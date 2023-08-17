ce8fd3ba918b30387777d1af37995ffa0b225537
Usama Enany <osama.enany@yahoo.com>
2023-08-17 14:39:36 -0700

    Comment Infinite

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN__H
+#define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -7,3 +10,5 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
+
+#endif /* MAIN_H */
