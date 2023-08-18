48b81d7beb93bc77db72ecccc476c4a4e9c5c723
Usama Enany <osama.enany@yahoo.com>
2023-08-18 05:13:10 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,13 @@
 
 void print_numbers(void)
 {
-	int i = '0';
+	char c = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 
-		if (i == '9')
+		if (c == '9')
 		{
 			_putchar('\n');
 		}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,3 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
-
-
