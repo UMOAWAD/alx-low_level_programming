eded125182647db2fa54f17c92e190b3bfa07d50
Usama Enany <osama.enany@yahoo.com>
2023-08-18 12:20:25 -0700

    Print Integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints an integer.
+ *
+ * @n: the integer to print.
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int i, x, c;
+
+	for (i = 10; i <= n; i = i * 10)
+	{
+	}
+
+	for (c = i / 10; c >= 1; c = c / 10)
+	{
+		x = n / c;
+
+		if (x >= 10)
+		{
+			x = x % 10;
+		}
+
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
