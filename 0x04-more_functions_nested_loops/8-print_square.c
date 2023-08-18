f9d1dd8914202f024ce8d8bf402e7c7adfe7e4a8
Usama Enany <osama.enany@yahoo.com>
2023-08-18 07:50:53 -0700

    Print Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,20 @@ void print_square(int size)
 {
 	int i, c;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (c = 0; c < size; c++)
-		{
-			_putchar(35);
-		}
-
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
 	}
 }
