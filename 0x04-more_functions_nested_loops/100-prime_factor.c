ea36dd7c9369a818f350aeff66ebf8d4cc5cb9fd
Usama Enany <osama.enany@yahoo.com>
2023-08-18 10:50:52 -0700

    Largest Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of a number.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long long int i = 612852475143;
+	int l;
+
+	for (l = i / 2; l >= 2; l--)
+	{
+		if (i % l == 0)
+		{
+			if (l % 2 != 0 && l % 3 != 0 && l % 5 != 0)
+			{
+				if (l % 7 != 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	printf("%d\n", l);
+
+	return (0);
+}
