5d4f0f3119cd3195fe4534a68e9dd91f13f7870b
Usama Enany <osama.enany@yahoo.com>
2023-08-18 06:28:59 -0700

    More Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the numbers, from 0 to 14, followed by new line.
- *
- * Return: the numbers.
- */
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  *
+  * Return: 10 times of the numbers since 0 up to 14
+  */
 
 void more_numbers(void)
 {
