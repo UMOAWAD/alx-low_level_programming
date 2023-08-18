e138c181b36d6d1cc896c2684334ce682f1c72f0
Usama Enany <osama.enany@yahoo.com>
2023-08-18 12:27:33 -0700

    Print Integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * main - prints an integer.
@@ -13,25 +12,25 @@ void print_number(int n)
 {
 	int i, x, c;
 
-	for (i = 10; i <= abs(n); i = i * 10)
-	{
-	}
-
 	if (n < 0)
 	{
 		_putchar('-');
+		
+		n *= -1;
+	for (i = 10; i <= n; i = i * 10)
+	{
 	}
 
 	for (c = i / 10; c >= 1; c = c / 10)
 	{
 		x = n / c;
 
-		if (abs(x) >= 10)
+		if (x >= 10)
 		{
 			x = x % 10;
 		}
 
-		_putchar(abs(x) + '0');
+		_putchar(x + '0');
 	}
 
 	_putchar('\n');
