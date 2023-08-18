8424586feebed98dce1ac43c93b4b34289a5bc9f
Usama Enany <osama.enany@yahoo.com>
2023-08-18 06:38:52 -0700

    Straight Line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,8 +10,17 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('-');
+		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('-');
+		}
+	}
+
+	_putchar('\n');
 }
