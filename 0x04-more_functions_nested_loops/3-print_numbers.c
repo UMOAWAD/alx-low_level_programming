7f84e97f24a563dfc50a9c8794edc927dc9a055c
Usama Enany <osama.enany@yahoo.com>
2023-08-18 05:05:58 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+
+		if (i == '9')
+		{
+			_putchar('\n');
+		}
+	}
+
+	return (0);
+}
