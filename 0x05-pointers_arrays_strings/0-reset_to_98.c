6589055c4efdd169a47cffb2aff21c4cc6ed7baa
Usama Enany <osama.enany@yahoo.com>
2023-08-21 11:03:36 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets the value of an integer to 98 through a pointer.
+ *
+ * @n: the pointer variable.
+ *
+ * Return: empty.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+A readme
+0-reset_to_98.c sets the value of the variable pointed to to 98.
+1-swap.c swaps the values of two integers
