0c66aac8ce0112d25991b8830c05be036fbf2e56
Usama Enany <osama.enany@yahoo.com>
2023-08-24 12:15:32 -0700

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @dest: the first string.
+ *
+ * @src: the string to append.
+ *
+ * @n: the maximum number of Bytes.
+ *
+ * Return: the resulting dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, x;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		if (x < n)
+		{
+			dest[i] = src[x];
+			i++;
+		}
+	}
+
+	if (x < n)
+	{
+		i++;
+		dest[i] = '\0';
+	}
+}
