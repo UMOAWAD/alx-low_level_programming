b2f9c0fffa1c119dcdedcb87aba0bdfd693d9e3c
Usama Enany <osama.enany@yahoo.com>
2023-08-29 19:56:16 -0700

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - find a character in a string.
+ *
+ * @s: the string to look in.
+ *
+ * @c: the character to find.
+ *
+ * Return: a pointer to the first occurrence of the character.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			p = &s[i]
+			return (p);
+		}
+	}
+
+	return (Null);
+}
