a8ebcde46484081d8d9c7284021a64a662a6e245
Usama Enany <osama.enany@yahoo.com>
2023-08-30 08:17:51 -0700

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,16 +13,15 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			p = &s[i];
-			return (p);
+			s += i;
+			return (s);
 		}
 	}
 
-	return s[i];
+	return ('\0');
 }
