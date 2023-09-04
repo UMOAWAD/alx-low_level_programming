8f6c44c794edcaffaa604bbd284f23ed36d25111
Usama Enany <osama.enany@yahoo.com>
2023-09-04 12:27:46 -0700

    String Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,42 +17,33 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, x = 0, c = 0, d = 0;
 	char *s;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i])
-	{
 		i++;
-	}
 
 	while (s2[x])
-	{
 		x++;
-	}
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
-		s2 = "";
-	}
 
 	s = malloc(sizeof(char) * (i + x) + 1);
 
 	if (s == NULL)
-	{
 		return ('\0');
-	}
 
 	for (c = 0; c < i; c++)
-	{
 		s[c] = s1[c];
-	}
 
 	for (d = 0; d < x; d++)
 	{
 		s[c] = s2[d];
 		c++;
 	}
+
+	s[c] = '\0';
 
 	return (s);
 }
