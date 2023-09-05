ea8151cbb65c07b08ec50a437c6f6135a6974062
Usama Enany <osama.enany@yahoo.com>
2023-09-05 12:13:13 -0700

    Strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words.
+ *
+ * @str: the string to split.
+ *
+ * Return: a pointer to an array of words.
+ */
+
+char **strtow(char *str)
+{
+}
