384eb18e33208a8fd2eaf20ce52e93317b7aeb99
Usama Enany <osama.enany@yahoo.com>
2023-09-07 10:37:53 -0700

    Malloc Checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory then checks if it is successful.
+ *
+ * @b: the size of memory to allocate.
+ *
+ * Return: 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *alloc = malloc(b);
+
+	if (alloc == NULL)
+	{
+		printf("98");
+		return (1);
+	}
+
+	return (alloc);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+A readme
+0-malloc_checked.c allocates memory, then checks if the allocation is successful.
