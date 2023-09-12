662767ca9ac31ea92cfbcec099761dd65e409971
Usama Enany <osama.enany@yahoo.com>
2023-09-12 12:40:15 -0700

    New Dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - defines a dog.
+ *
+ * @name: its name.
+ *
+ * @age: its age,
+ *
+ * @owner: its owner.
+ *
+ * Return: void.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+}
