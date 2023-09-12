d3da229202b971c7d9d996196c3f64d4a3977fba
Usama Enany <osama.enany@yahoo.com>
2023-09-12 09:53:20 -0700

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
