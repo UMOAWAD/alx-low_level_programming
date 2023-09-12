906d9d885586ade04974ed8ab2c99296ae4bf5a7
Usama Enany <osama.enany@yahoo.com>
2023-09-12 11:14:32 -0700

    First Struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+/**
+ * struct dog - defines a dog.
+ *
+ * @name: the name of the dog.
+ *
+ * @age: the age of the dog.
+ *
+ * @owner: the owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
