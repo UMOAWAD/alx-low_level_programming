4e43b476d5864d577c458cea6c396e17f4b5d1ce
Usama Enany <osama.enany@yahoo.com>
2023-09-12 11:55:20 -0700

    Init Dog#

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,15 +13,15 @@
  *
  * @owner: the owner of the dog.
  *
- * Return: 0.
+ * Return: void.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d -> name = name;
-		d -> age = age;
-		d -> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
