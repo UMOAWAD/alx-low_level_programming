747cf9efc1cb1f0707601939fde5e60462edc7d6
Usama Enany <osama.enany@yahoo.com>
2023-09-15 12:16:38 -0700

    Opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - prints the opcodes of itself.
+ *
+ * @argc: The length of the arguments array.
+ *
+ * @argv: The arguments array.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	return (0);
