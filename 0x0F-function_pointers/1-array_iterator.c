f7f32fed62782c32009cface9572cc1051bbd106
Usama Enany <osama.enany@yahoo.com>
2023-09-14 21:32:51 -0700

    Array Iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,6 +5,8 @@
 /**
  * array_iterator - runs a function on each element of of array.
  *
+ * @array: the array in question.
+ *
  * @size: the size of the array.
  *
  * @action: the function to run.
@@ -16,7 +18,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action == NULL)
+	if (array = NULL || action == NULL)
 	{
 		return;
 	}
